491ccc8bd8c8f54c45b940e5f389e0cabeda8391
Alexandr <aleksandr.zanko@enway.com>
2019-05-27 11:51:50 +0300

    add executive dependencies

--- sfz.js
+++ sfz.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
+const exec = require('executive');
+
 const args = process.argv;
 console.log('Hello World!', args);
+
+exec([
+    'npm -v',
+    'node -v',
+    'sfdx -v'
+]);
